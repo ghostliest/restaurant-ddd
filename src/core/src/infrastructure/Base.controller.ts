eac78b6b62cc5aefd74f2cce97e2db69587ebca1
ghostliest <LukEv0nz@gmail.com>
2023-09-27 17:02:24 +0300

    Modified: base controller in core

--- src/core/src/infrastructure/Base.controller.ts
+++ src/core/src/infrastructure/Base.controller.ts
@@ -16,4 +16,9 @@ export class BaseController {
     console.log("[BaseController - conflict]: ", error);
     return res.status(HttpStatus.CONFLICT).json(error);
   }
+
+  public notFound(res: Response, error: Error | string | any) {
+    console.log("[BaseController - notFound]: ", error);
+    return res.status(HttpStatus.NOT_FOUND).json(error);
+  }
 }
